0559debacac1a6fd7132b202bf56871c6190e0fd
marinofelipe <felipemarino91@gmail.com>
2017-01-09 21:46:29 -0200

    Pod install
    All dependencies installed and xcworkspace generated

---
+++ Pods/Target Support Files/Pods-TDD Game Of Life-TDD Game Of LifeTests/Pods-TDD Game Of Life-TDD Game Of LifeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TDD_Game_Of_Life_TDD_Game_Of_LifeTests : NSObject
+@end
+@implementation PodsDummy_Pods_TDD_Game_Of_Life_TDD_Game_Of_LifeTests
+@end
---
+++ Pods/Target Support Files/Pods-TDD Game Of Life-TDD Game Of LifeUITests/Pods-TDD Game Of Life-TDD Game Of LifeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TDD_Game_Of_Life_TDD_Game_Of_LifeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TDD_Game_Of_Life_TDD_Game_Of_LifeUITests
+@end
